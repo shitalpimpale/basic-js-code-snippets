5b818426bfa05d96900dbc16a2f0ec2d5c4d4112
shitalpimpale <pimpale.shital@gmail.com>
2024-10-22 22:22:42 +0530

    Update non-repeating-character.js

--- non-repeating-character.js
+++ non-repeating-character.js
@@ -1,15 +1,11 @@
 function firstNonRepeatingChar(str) {
-
     const charCount = {};
-
     for (const char of str) {
         charCount[char] = (charCount[char] || 0) + 1;
-
     }
     for (const char of str) {
         if (charCount[char] === 1) {
             return char;
-
         }
     }
     return null;
