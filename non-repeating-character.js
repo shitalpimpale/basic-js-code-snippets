ba9a62899c204db625c43b25f7540238c77c4b42
shpi1 <shpi@ciklum.com>
2024-10-22 17:16:33 +0530

    first non repreating character in string

---
+++ non-repeating-character.js
@@ -0,0 +1,19 @@
+function firstNonRepeatingChar(str) {
+
+    const charCount = {};
+
+    for (const char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
+
+    }
+    for (const char of str) {
+        if (charCount[char] === 1) {
+            return char;
+
+        }
+    }
+    return null;
+}
+
+// Example usage:
+console.log(firstNonRepeatingChar("hello"));  //output : h
